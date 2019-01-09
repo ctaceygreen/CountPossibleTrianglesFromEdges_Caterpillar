dec367eefecf7f236ea09bbaef6d9e3799a40dd3
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 12:22:43 +0000

    Add project files.

---
+++ CountPossibleTrianglesFromEdges_Caterpillar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+        int result = 0;
+        Array.Sort(A);
+        for(int x = 0; x < A.Length; x++)
+        {
+            int z = x + 2;
+            for(int y = x + 1; y < A.Length; y++)
+            {
+                while(z < A.Length && A[x] + A[y] > A[z])
+                {
+                    z += 1;
+                }
+                result += z - y - 1;
+            }
+        }
+        return result;
+    }
+}
